f825d6c639dbe7e02caf693b8a3eb95ed6dd3255
JakeMass <jakob.massem@gmx.de>
2022-08-18 20:16:26 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use little_server::ThreadPool;
+use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    loop {
+        for stream in listener.incoming() {
+            let stream = stream.unwrap();
+    
+            pool.execute(|| {
+                handle_connection(stream);
+            });
+
+            println!("running loop");
+        }
+    }
+
+    println!("Shutting down.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let js = b"GET /test.js HTTP/1.1\r\n";
+
+    let (status_line, filename) = 
+        if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK", "hello.html")
+        } else if buffer.starts_with(js) {
+            ("HTTP/1.1 200 OK\r\nX-Content-Type-Options: nosniff\r\nContent-Type: text/javascript", "test.js")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
